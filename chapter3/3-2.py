b9ec18ab68a4b3223624ea28672368197b464803
조영준 <shootinger@naver.com>
2024-10-30 00:55:31 +0900

    3-2 까지

--- chapter3/3-2.py
+++ chapter3/3-2.py
@@ -6,3 +6,39 @@ while treeHit < 10:
        print("나무 넘어갑니다.")
 
 
+
+prompt = """
+ 1. Add
+ 2. Del
+ 3. List
+ 4. Quit
+
+Enter number: """
+
+
+number = 0
+while number != 4:
+     print(prompt)
+     number = int(input())
+
+
+coffee = 10
+money = 300
+while money:
+     print("돈을 받았으니 커피를 줍니다.")
+     coffee = coffee -1
+     print("남은 커피의 양은 %d개입니다." % coffee)
+     if coffee == 0:
+         print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+         break
+
+
+#continue
+a = 0
+while a < 10:
+     a = a + 1
+     if a % 2 == 0: 
+         continue
+     print(a)
+
+     
